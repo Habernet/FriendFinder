e2af8a797d6b3eee730e7fc9ed204acf70f78559
habernet <phaberern@gmail.com>
2019-06-09 17:01:52 -0400

    htmlroutes complete

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,12 +2,14 @@ const path = require("path");
 
 module.exports = (app) => {
     // set the HTML routes here for exporting to the server.js
+
+    // Get route that will serve the survey page
     app.get('/survey', (req, res) => {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
     
-    //Defaul catch all route that serves the home.html page
-    // app.get('*', (req, res) => {
-    //     res.sendFile(path.join(__dirname, "../public/home.html"));
-    // });
+    // Defaul catch all route that serves the home.html page
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
 };
